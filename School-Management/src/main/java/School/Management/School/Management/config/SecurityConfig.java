9293fa5c7a16db1b38551c98a3684b213c146819
Pravin06102005 <yadavpravin4444@gmail.com>
2025-12-12 00:20:58 +0530

    Update SecurityConfig.java

--- School-Management/src/main/java/School/Management/School/Management/config/SecurityConfig.java
+++ School-Management/src/main/java/School/Management/School/Management/config/SecurityConfig.java
@@ -14,6 +14,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import java.util.List;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 
 @Configuration
