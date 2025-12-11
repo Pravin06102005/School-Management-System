6ead01d7c79786a342f15932a655fdda7b3e2717
Pravin06102005 <yadavpravin4444@gmail.com>
2025-12-12 00:35:27 +0530

    Update CorsConfig.java

--- School-Management/src/main/java/School/Management/School/Management/config/CorsConfig.java
+++ School-Management/src/main/java/School/Management/School/Management/config/CorsConfig.java
@@ -1,30 +1,30 @@
-package School.Management.School.Management.config;
+// package School.Management.School.Management.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig {
+// @Configuration
+// public class CorsConfig {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
+//     @Bean
+//     public WebMvcConfigurer corsConfigurer() {
+//         return new WebMvcConfigurer() {
+//             @Override
+//             public void addCorsMappings(CorsRegistry registry) {
 
-                registry.addMapping("/**")
-                    .allowedOrigins(
-                            "https://school-management-s.netlify.app",
-                            "https://fit-gym1.netlify.app",
-                            "http://localhost:3000",
-                            "http://localhost:5173"
-                        )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
-}
+//                 registry.addMapping("/**")
+//                     .allowedOrigins(
+//                             "https://school-management-s.netlify.app",
+//                             "https://fit-gym1.netlify.app",
+//                             "http://localhost:3000",
+//                             "http://localhost:5173"
+//                         )
+//                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+//                         .allowedHeaders("*")
+//                         .allowCredentials(true);
+//             }
+//         };
+//     }
+// }
