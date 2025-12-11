510a0133fccba93ed20e01fbac54a15d4069f56a
Pravin06102005 <yadavpravin4444@gmail.com>
2025-12-12 00:05:27 +0530

    Add CrossOrigin annotation to StudentController

--- School-Management/src/main/java/School/Management/School/Management/controller/StudentController.java
+++ School-Management/src/main/java/School/Management/School/Management/controller/StudentController.java
@@ -18,6 +18,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/students")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class StudentController {
 
     private final StudentService studentService;
