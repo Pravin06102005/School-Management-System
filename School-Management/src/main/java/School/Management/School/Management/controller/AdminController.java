e400090460515ec87b7702b3bbc3e01e99e67177
Pravin06102005 <yadavpravin4444@gmail.com>
2025-12-12 00:03:55 +0530

    Add CrossOrigin annotation to AdminController

--- School-Management/src/main/java/School/Management/School/Management/controller/AdminController.java
+++ School-Management/src/main/java/School/Management/School/Management/controller/AdminController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/admin")
+@CrossOrigin(origins = "*")
 public class AdminController {
 
     private final AdminService adminService;
